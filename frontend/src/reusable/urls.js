61d825418a7b58d90a33d4e1020d8cd8620b1b1b
Maria Sparre <you@example.com>
2021-05-27 12:59:50 +0200

    changed heroku link

--- frontend/src/reusable/urls.js
+++ frontend/src/reusable/urls.js
@@ -1,4 +1,4 @@
 // const BASE_URL = 'http://localhost:8080'
 // const mongoUrl = process.env.MONGO_URL || `mongodb+srv://${process.env.USER_ID}:${process.env.API_KEY}@cluster0.bvwog.mongodb.net/shoppingList?retryWrites=true&w=majority`
-const BASE_URL = 'https://app-shopping-notes.herokuapp.com/'
+const BASE_URL = 'https://app-shopping-notes.herokuapp.com'
 export const API_URL = (slug) => `${BASE_URL}/${slug}`  
